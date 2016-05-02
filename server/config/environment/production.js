2c7b74d44c1ea0c192965cdf78b27eade53db655
Annie Luc <annhiluc@gmail.com>
2016-05-02 12:24:12 -0400

    Deploying first draft

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -3,7 +3,7 @@
 // Production specific configuration
 // =================================
 module.exports = {
-  // Server IP
+  /*// Server IP
   ip:     process.env.OPENSHIFT_NODEJS_IP ||
           process.env.IP ||
           undefined,
@@ -20,5 +20,13 @@ module.exports = {
           process.env.OPENSHIFT_MONGODB_DB_URL +
           process.env.OPENSHIFT_APP_NAME ||
           'mongodb://localhost/kidscount'
-  }
+  }*/
+
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://dev:dev@ds015760.mlab.com:15760/kids-count'
+  },
+
+  // Seed database on startup
+  seedDB: true
 };
