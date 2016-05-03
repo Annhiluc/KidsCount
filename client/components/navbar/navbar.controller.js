08390945fca80caeba0388f52ae7a199496b8b5e
Annie Luc <annhiluc@gmail.com>
2016-05-03 14:40:10 -0400

    Fixing first deployment

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('kidsCountApp')
-  .controller('NavbarController', ['$scope', '$timeout', '$location','Auth', function($scope, $timeout, $location, Auth){
+  .controller('NavbarController', ['$scope', function($scope){
     $scope.menu = [{
       'title': 'Home',
       'state': 'main'
@@ -37,37 +37,7 @@ angular.module('kidsCountApp')
 
     $scope.isCollapsed = true;
 
-    $timeout(function(){
-      $(".dropdown-button").dropdown();
-      $('.button-collapse').sideNav({
-            closeOnClick: false,
-            edge: 'right'
-          }
-      );
-      $('.collapsible').collapsible();
-    });
-
     $scope.isLoggedIn = Auth.isLoggedIn;
     $scope.isAdmin = Auth.isAdmin;
     $scope.getCurrentUser = Auth.getCurrentUser;
-
-    // See what route is active
-    $scope.routeIs = function(item){
-      if($location.path() == item){
-        return true;
-      }
-      return false;
-    }
-
-    $scope.routeContains = function(item){
-      if($location.path().indexOf(item) != -1){
-        return true;
-      }
-      return false;
-    }
-
-
-    $scope.closeNav = function(){
-      $('.button-collapse').sideNav('hide');
-    }
 }]);
