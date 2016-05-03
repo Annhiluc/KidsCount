8c4634223ef3f98441f07ef2c3e1cd2cab3cc27d
Annie Luc <annhiluc@gmail.com>
2016-05-03 14:38:22 -0400

    Fixing first deployment

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -1,49 +1,73 @@
 'use strict';
 
-class NavbarController {
-  //start-non-standard
-  menu = [{
-    'title': 'Home',
-    'state': 'main'
-  },
-  {
-    'title': 'About Us',
-    'state': 'about'
-  },
-  {
-    'title': 'Volunteer',
-    'state': 'volunteer'
-  },
-  {
-    'title': 'Ways to Give',
-    'state': 'give'
-  },
-  {
-    'title': 'Contact',
-    'state': 'contact'
-  },
-  {
-    'title': 'Resources',
-    'state': 'resources'
-  },
-  {
-    'title': 'Gallery',
-    'state': 'gallery'
-  },
-  {
-    'title': 'Blog',
-    'state': 'blog'
-  }];
-
-  isCollapsed = true;
-  //end-non-standard
-
-  constructor(Auth) {
-    this.isLoggedIn = Auth.isLoggedIn;
-    this.isAdmin = Auth.isAdmin;
-    this.getCurrentUser = Auth.getCurrentUser;
-  }
-}
-
 angular.module('kidsCountApp')
-  .controller('NavbarController', NavbarController);
+  .controller('NavbarController', ['$scope', '$timeout', '$location','Auth', function($scope, $timeout, $location, Auth){
+    $scope.menu = [{
+      'title': 'Home',
+      'state': 'main'
+    },
+    {
+      'title': 'About Us',
+      'state': 'about'
+    },
+    {
+      'title': 'Volunteer',
+      'state': 'volunteer'
+    },
+    {
+      'title': 'Ways to Give',
+      'state': 'give'
+    },
+    {
+      'title': 'Contact',
+      'state': 'contact'
+    },
+    {
+      'title': 'Resources',
+      'state': 'resources'
+    },
+    {
+      'title': 'Gallery',
+      'state': 'gallery'
+    },
+    {
+      'title': 'Blog',
+      'state': 'blog'
+    }];
+
+    $scope.isCollapsed = true;
+
+    $timeout(function(){
+      $(".dropdown-button").dropdown();
+      $('.button-collapse').sideNav({
+            closeOnClick: false,
+            edge: 'right'
+          }
+      );
+      $('.collapsible').collapsible();
+    });
+
+    $scope.isLoggedIn = Auth.isLoggedIn;
+    $scope.isAdmin = Auth.isAdmin;
+    $scope.getCurrentUser = Auth.getCurrentUser;
+
+    // See what route is active
+    $scope.routeIs = function(item){
+      if($location.path() == item){
+        return true;
+      }
+      return false;
+    }
+
+    $scope.routeContains = function(item){
+      if($location.path().indexOf(item) != -1){
+        return true;
+      }
+      return false;
+    }
+
+
+    $scope.closeNav = function(){
+      $('.button-collapse').sideNav('hide');
+    }
+}]);
